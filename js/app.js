a638a12ad6195aff39a1091018e62c8d9f516912
ThiagoMoraless <thiagomorales45@gmail.com>
2023-06-05 20:08:50 -0300

    PrimerEjercicio

---
+++ js/app.js
@@ -0,0 +1,29 @@
+console.log("Aguante All Boys")
+/*Variable cajon donde voy a guardar algo (puede ser numero, letra o lo que yo quiera)*/
+/*Cuando le pongo = a algo, ese suceso se llama asignación */
+const Numeroqueinvente = 2; /*Number*/
+console.log(Numeroqueinvente)
+const Letraqueinvente = "T"/*String*/
+console.log(Letraqueinvente) 
+/*Const= variable que es constante y no cambia */
+/*Let y Var son variables que si cambian */
+let Nombre = "Pepe"
+console.log(Nombre)
+Nombre = "Tito"
+console.log(Nombre)
+/*El tipo de variable que puede ser const let o var mas el nombre de la variable, se le llama declaración */
+let Thiago;
+Thiago = 5
+console.log(Thiago)
+let NumeroA = 1
+let NumeroB = 2
+let NumeroC = 3
+console.log(NumeroA + NumeroB + NumeroC)
+let TextoA = "Mauri"
+let TextoB = "Cucco"
+console.log(TextoA +" " +TextoB)
+/*Prompt te permite ingresar lo que vos quieras y lo guarda en una variable (En este caso en NombreIngresado) */
+alert("Bienvenido a la página de Surf mas famosa del país")
+let NombreIngresado = prompt("Ingrese su nombre");
+console.log(NombreIngresado)
+alert("Bienvenido " + NombreIngresado)
